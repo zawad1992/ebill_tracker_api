c68ff14c004d6b571bc230c09f7d0d7a8101afed
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-04-06 04:49:42 +0600

    Add last_activity field to users table migration

---
+++ database/migrations/2024_04_05_210510_add_last_activity_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->date('last_activity')->nullable()->default(Carbon::now())->after('remember_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('last_activity');
+        });
+    }
+};
