0ff3a72b806c43d14dd7676e9c57e72ad7493d78
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-04-07 02:11:56 +0600

    Refactor user authentication routes and functionality in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,31 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\JsonResponse;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
-
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::get('/healthcheck', function () {
     return response()->json(['status' => 'ok']);
 });
 
-
+Route::post('/user/register', 'App\Http\Controllers\AuthenitcationsController@register');
 Route::post('/user/authenticate', 'App\Http\Controllers\AuthenitcationsController@authenticate');
 
 Route::middleware('auth:sanctum', 'last.activity')->group(function () {
-    Route::get('/user', 'App\Http\Controllers\UsersController@userinfo');
     Route::post('/user/logout', 'App\Http\Controllers\AuthenitcationsController@logout');
+    Route::get('/user', 'App\Http\Controllers\UsersController@userinfo');
+
+    Route::get('/billlogs/types', 'App\Http\Controllers\BillLogsController@getBillLogType');
+    Route::get('/billlogs', 'App\Http\Controllers\BillLogsController@index');
+    Route::get('/billlogs/{id}', 'App\Http\Controllers\BillLogsController@show');
+    Route::post('/billlogs/add', 'App\Http\Controllers\BillLogsController@store');
+    Route::put('/billlogs/edit/{id}', 'App\Http\Controllers\BillLogsController@update');
+    Route::delete('/billlogs/delete/{id}', 'App\Http\Controllers\BillLogsController@destroy');
+
 });
