d96b312a6e905e6d426867a5f7b3f832939944cb
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-04-06 22:57:11 +0600

    Refactor user authentication routes and functionality in api.php

--- routes/api.php
+++ routes/api.php
@@ -23,37 +23,10 @@ Route::get('/healthcheck', function () {
     return response()->json(['status' => 'ok']);
 });
 
-Route::post('/user/authenticate', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ], [
-        'email.required' => 'Email is required',
-        'email.email' => 'Email is invalid',
-        'password.required' => 'Password is required',
-        'device_name.required' => 'Device name is required',
-    ]);
-   
-    
-    $user = User::where('email', $request->email)->first();
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-    $user->update(['last_activity' => now()->format('Y-m-d')]);
-    
-    return $user->createToken($request->device_name)->plainTextToken;
-});
+
+Route::post('/user/authenticate', 'App\Http\Controllers\AuthenitcationsController@authenticate');
 
 Route::middleware('auth:sanctum', 'last.activity')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::post('/user/logout', function (Request $request) {
-        $request->user()->tokens()->delete();
-        return response()->json(['message' => 'Logged out']);
-    });
+    Route::get('/user', 'App\Http\Controllers\UsersController@userinfo');
+    Route::post('/user/logout', 'App\Http\Controllers\AuthenitcationsController@logout');
 });
