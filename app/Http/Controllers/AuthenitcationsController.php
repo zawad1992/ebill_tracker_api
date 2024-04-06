8614a95ecac4d921d9a399293218bf01ba429a4f
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-04-07 02:10:56 +0600

    Add user registration functionality to AuthenitcationsController.php

--- app/Http/Controllers/AuthenitcationsController.php
+++ app/Http/Controllers/AuthenitcationsController.php
@@ -4,14 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
 
+use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\AuthenticationRequest;
+
+use App\Models\User;
+
 
 class AuthenitcationsController extends Controller
 {
+    public function register(RegisterRequest $request)
+    {
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->email_verified_at = now();
+        $user->last_activity = now()->format('Y-m-d');
+        $user->remember_token = Str::random(10);
+        $user->save();
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        $user->token = $token;
+
+        return response()->json($user, 201);
+    }
+
     public function authenticate(AuthenticationRequest $request)
     {
         $user = User::where('email', $request->email)->first();
