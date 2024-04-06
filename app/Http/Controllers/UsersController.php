7d0f1cf23cd99de4deb99eb8f5e871fdbb8cc10e
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-04-07 02:11:13 +0600

    Update UsersController with userinfo method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class UsersController extends Controller
 {
     public function userinfo(Request $request)
     {
         return $request->user();
     }
+
 }
